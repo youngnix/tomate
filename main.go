6889d1b62cc8f3d39a9f5d0eb938b0a7dc7061b9
Andrei de Souza <71538656+youngnix@users.noreply.github.com>
2024-10-05 12:38:51 -0300

    feat: functional bare-bones timer

--- main.go
+++ main.go
@@ -1,24 +1,73 @@
 package main
 
 import (
+	"fmt"
+	"time"
+
 	"fyne.io/fyne/v2/app"
 	"fyne.io/fyne/v2/container"
+	"fyne.io/fyne/v2/data/binding"
 	"fyne.io/fyne/v2/layout"
 	"fyne.io/fyne/v2/widget"
+)
+
+const (
+	TIMER_STATE_PAUSED = iota
+	TIMER_STATE_RUNNING
 )
 
 func main() {
 	myApp := app.New()
 	appWindow := myApp.NewWindow("Tomate")
 
-	countdown_label := widget.NewLabel("TO BE SET")
+	var countdown int
+	state := TIMER_STATE_PAUSED
+
+	countdown_string := binding.NewString()
+
+	SetCountdown := func(c int) {
+		countdown = c
+		countdown_string.Set(fmt.Sprintf("%02d:%02d", countdown/60, countdown%60))
+
+		if countdown == 0 {
+			myApp.Quit()
+		}
+	}
+
+	SetCountdown(60 * 60)
+
+	var start_pause_button *widget.Button
+	countdown_label := widget.NewLabelWithData(countdown_string)
+
+	countdown_func := func() {
+		for range time.Tick(time.Second) {
+			if state != TIMER_STATE_RUNNING {
+				return
+			}
+
+			SetCountdown(countdown - 1)
+		}
+	}
+
+	start_pause_button = widget.NewButton("Start", func() {
+		switch state {
+		case TIMER_STATE_PAUSED:
+			state = TIMER_STATE_RUNNING
+			start_pause_button.SetText("Pause")
+			go countdown_func()
+			break
+		case TIMER_STATE_RUNNING:
+			state = TIMER_STATE_PAUSED
+			start_pause_button.SetText("Start")
+			break
+		}
+	})
 
 	appWindow.SetContent(
 		container.New(layout.NewCenterLayout(),
 			container.New(layout.NewVBoxLayout(),
 				countdown_label,
-				widget.NewButton("Start", func() {
-				}),
+				start_pause_button,
 			),
 		),
 	)
